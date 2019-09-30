3a8b9891f710580a6985ce17d234fe148f64372a
Jose Galindo <jgalindo.ironhack@gmail.com>
2019-09-30 15:58:48 -0400

    started code set up

---
+++ starter-code/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      your code and components go here
+    </div>
+  );
+}
+
+export default App;
